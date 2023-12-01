c9dad25e4b31dbb80b66c85e460f976ef0c6c15c
Christopher Harris <cbrentharris@gmail.com>
2023-12-01 10:24:50 -0600

    Further refactoring

--- src/main/kotlin/Day1.kt
+++ src/main/kotlin/Day1.kt
@@ -2,25 +2,20 @@ import kotlin.String
 import kotlin.collections.List
 
 object Day1 {
-    fun part1(input: List<String>): String {
-        return input.sumOf { i -> findFirstAndLastDigit(i) }.toString()
-    }
+    fun part1(input: List<String>): String = input.sumOf(::findFirstAndLastDigit).toString()
 
     private fun findFirstAndLastDigit(i: String): Int {
         val digits = i.filter { it.isDigit() }
         return (digits.take(1) + digits.takeLast(1)).toInt()
     }
 
-    fun part2(input: List<String>): String {
-        return input.sumOf { i -> findFirstAndLastDigitV2(i) }.toString()
-    }
+    fun part2(input: List<String>): String = input.sumOf(::findFirstAndLastDigitV2).toString()
 
     private fun findFirstAndLastDigitV2(i: String): Int {
         val words = listOf("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
-        val newI = words.zip(words.indices).fold(i) { acc, (word, index) ->
-            val replacement = word.first() + index.toString() + word.last()
-            acc.replace(word, replacement)
-        }
+        val newI = words.zip(words.indices)
+            .map { (word, index) -> word to word.first() + index.toString() + word.last() }
+            .fold(i) { acc, (word, replacement) -> acc.replace(word, replacement) }
         return findFirstAndLastDigit(newI)
     }
 }
