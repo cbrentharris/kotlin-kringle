f41d070673c0be2353b2bc1353d109f006ab9aa6
Christopher Harris <cbrentharris@gmail.com>
2023-12-06 09:40:52 -0600

    More comments and refactoring

--- src/main/kotlin/Day6.kt
+++ src/main/kotlin/Day6.kt
@@ -30,6 +30,7 @@ object Day6 {
             .replace(" ", "")
             .toLong()
         val (maxTime, minTime) = minAndMaxTimes(time, distance)
+        // Add 1 to count the upper as inclusive
         return (maxTime - minTime + 1).toString()
     }
 
@@ -40,12 +41,14 @@ object Day6 {
      * 0 = -holdTime^2 + time * holdTime - distance
      *
      * We can then use the quadratic formula to find the holdTime:
-     * holdTime = (-time +- sqrt(time^2 - 4 * -distance)) / -2
+     * holdTime = (-time +- sqrt(time^2 - 4 * -(distance + 1))) / -2
+     *
+     * Note, we set distance + 1 because we have to exceed the target distance
      */
     private fun minAndMaxTimes(time: Long, distance: Long): Pair<Long, Long> {
         val upper = (-time - sqrt(time.toDouble().pow(2) - 4 * (distance + 1))) / -2
         val lower = (-time + sqrt(time.toDouble().pow(2) - 4 * (distance + 1))) / -2
         // We have to floor the upper because we cannot hold fractional seconds, and have to ceil the lower
-        return Pair(floor(upper).toLong(), ceil(lower).toLong())
+        return floor(upper).toLong() to ceil(lower).toLong()
     }
 }
